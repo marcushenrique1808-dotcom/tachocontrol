3f0cccf16d9a6cd0e9b61821c6249f32e44b5b00
marcushenrique1808-dotcom <marcushenrique1808@gmail.com>
2026-09-17 21:41:22 +0100

    Adiciona service worker PWA

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE="tachocontrol-v1-13-4-gh1";
+const FILES=["./","./index.html","./manifest.webmanifest","./src/app.js","./icons/icon.svg"];
+
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("message",e=>{if(e.data?.type==="SKIP_WAITING")self.skipWaiting()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",x));return r}).catch(()=>caches.match("./index.html")));return}e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{if(r?.ok){const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x))}return r}).catch(()=>cached)))});
